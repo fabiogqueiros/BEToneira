6e1196b3e42e7988439194d4d9c834638cc52070
arthurbougleux <arthur_lbs@id.uff.br>
2024-06-27 21:32:44 -0300

    Brilha Fábio

---
+++ src/main/java/betoneira/betoneira/model/recovery/RecuperacaoEmail.java
@@ -0,0 +1,13 @@
+package betoneira.betoneira.model.recovery;
+
+import betoneira.betoneira.model.recovery.Recuperacao;
+
+public class RecuperacaoEmail extends {
+    private String email;
+    protected Integer codigo;
+    
+    
+    public void enviarEmail(){};
+    public void gerarCodigo(){};
+    public void confirmarCodigo(String codigo){};
+}
